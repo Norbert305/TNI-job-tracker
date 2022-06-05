2eadb37799e37c2155894532d151453c7b873c2d
Imani Taylor <imanitay@Imanis-MacBook-Air.local>
2022-06-04 20:59:01 -0500

    adding map to pages

--- client/src/components/AboutPage.js
+++ client/src/components/AboutPage.js
@@ -1,12 +1,15 @@
 import React from "react";
-
+import {StyledBanner4, StyledBanner5} from "./styled/Banner.styled";
 
 export function AboutPage () {
 
 return (<div className="text-center">
-
-    <h1>About Us</h1>
-
+    <StyledBanner4>
+    <h1 className="BannerText">About Us</h1>
+    </StyledBanner4>
+    <StyledBanner5>
+    <h4 className="BannerText">We help people find jobs with our concreate strategized approach using our famous job tracker</h4>
+    </StyledBanner5>
 </div>)
 
 
